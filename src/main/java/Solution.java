7b27d4097a6a2af4fceead7c37bc243d726a1d2c
Gregory664 <gregory664@yandex.ru>
2018-11-08 16:54:22 +0300

    Первый junit тест

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,12 @@
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static String Hello() {
+        return "Hello";
+    }
+
+    public static String HelloAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException();
+        }
+        return String.format("Hello, %d!", age);
     }
 }
---
+++ src/test/java/TestClass.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestClass {
+
+    @Test
+    public void TestHello() {
+          assertEquals("Hello", Solution.Hello());
+    }
+}
